9f83b9e457a076c34dc46998446ad4665a6b2dbf
tchewik <elenachistov@gmail.com>
2016-08-24 15:28:19 +0300

    implementing OAuth

--- tchotchka_with_bot/urls.py
+++ tchotchka_with_bot/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'tchotchka_with_bot.views.home', name='home'),
     url(r'^conversation$', 'tchotchka_with_bot.views.conversation', name='conversation'),
+    url('', include('social.apps.django_app.urls', namespace='social')),
 ]
